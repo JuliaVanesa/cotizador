232634beb065d121ed0c54dcbaea120a8e463c22
Julia Gonzalez <vanesagonz22@gmail.com>
2021-03-04 16:16:50 -0300

    commit

---
+++ src/helpers.js
@@ -0,0 +1,33 @@
+export function obtenerDiferenciaYear (year) {
+    return new Date().getFullYear() - year;
+}
+
+
+export function calcularMarca (marca) {
+    let incremento;
+
+    // eslint-disable-next-line default-case
+    switch (marca) {
+        case 'europeo': 
+            incremento = 1.30;
+            break;
+        case 'americano':
+            incremento = 1.15;
+            break;
+        case 'asiatico': 
+            incremento = 1.05;
+            break;
+        default:
+            break;
+
+    }
+    return incremento;
+}
+
+export function obtenerPlan (plan) {
+    return (plan === 'basico') ? 1.20 : 1.50;
+}
+
+export function primerMayuscula (texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1)
+}
